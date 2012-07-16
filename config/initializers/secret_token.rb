afcaf33acee585da9ebfbdc40d416734fb401b64
Giovanni Coia <giovanni@thecoia.com>
2012-07-16 16:02:12 +0100

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Iact::Application.config.secret_token = 'f4dbd025d69492aa5e398f40a762d6e7f7808f2b096b9af5defd1d820f7956aaaae8e11f6c9669f31883f129516435389f03ede9be17e42c949a5308dbad7cee'
